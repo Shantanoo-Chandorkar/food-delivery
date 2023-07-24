a3dd87c8132be9110ece0a7b99bcb7f64e9953d5
Shantanoo Chandorkar <chandorkarshantanoo@gmail.com>
2023-07-24 14:42:13 +0530

    commit-1

--- api/index.js
+++ api/index.js
@@ -5,14 +5,14 @@ const port = process.env.PORT || 5000;
 
 const mongoDB = require("./db");
 
-app.use((req, res, next) => {
-  // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 app.use(express.json());
 
 const userRoutes = require("./routes/UserRoutes");

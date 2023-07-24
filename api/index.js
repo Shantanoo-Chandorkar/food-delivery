348f7ef89d1e50f47511f1b04acf818f35393690
Shantanoo Chandorkar <chandorkarshantanoo@gmail.com>
2023-07-24 16:42:28 +0530

    modified: app.js

--- api/index.js
+++ api/index.js
@@ -5,18 +5,18 @@ const port = process.env.PORT || 5000;
 
 const mongoDB = require("./db");
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    // "https://sweetery-shop.netlify.app"
-    "http://localhost:3000"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     // "https://sweetery-shop.netlify.app"
+//     "http://localhost:3000"
+//   );
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.use(express.json());
 

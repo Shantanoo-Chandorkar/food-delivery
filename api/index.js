4a734f0a7a32f59cd7f29045837116a945a70e3a
Shantanoo Chandorkar <chandorkarshantanoo@gmail.com>
2023-07-24 17:29:02 +0530

    c9

--- api/index.js
+++ api/index.js
@@ -11,10 +11,7 @@ app.use((req, res, next) => {
     // "https://64be659796d6bd077f82ee93--nimble-platypus-8b683c.netlify.app"
     "http://localhost:3000"
   );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+
   next();
 });
 

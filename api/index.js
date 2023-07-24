7f7a0c463f6ae3fa7fc8b0b61166b5a58952ceeb
Shantanoo Chandorkar <chandorkarshantanoo@gmail.com>
2023-07-24 17:32:58 +0530

    c3

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
+const port = "https://easy-seal-culottes.cyclic.app";
 
 const mongoDB = require("./db");
 
